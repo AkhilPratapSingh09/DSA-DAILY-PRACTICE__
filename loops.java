e5c3d9de6fbd208f21ba40dafd8c76b078741d7b
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-04-08 21:23:56 +0530

    .

---
+++ loops.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class loops {
+    public static void  main(String []args){
+        // Scanner scn=new Scanner(System.in);
+        // int n = scn.nextInt();
+
+    //     for(int i=1; i<=9; i++){
+    //         System.out.println(i);
+    //     }
+    //     System.out.println("Done");
+    int i=1;
+    while(i<=9){
+        System.out.println(i);
+        i++;
+    }
+    }
+}
---
+++ prime.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class prime {
+    public static void main(String []args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int count=0;
+        for(int div=2; div*div <=n; div++){
+            if(n%div==0){
+                count++;
+                break;
+            }
+        }
+        if(count==0){
+            System.out.println("prime");
+
+        }else{
+            System.out.println("not prime");
+        }
+    }
+}
