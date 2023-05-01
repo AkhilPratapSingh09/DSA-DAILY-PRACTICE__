d6fb103828efbb032ffbd151aa906c45290322bb
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-05-01 09:31:38 +0530

    .

---
+++ BalancedBracket.java
@@ -0,0 +1,20 @@
+// import java.util.*;
+// public class BalancedBracket {
+//     public static void main(String[]args)throws Exception{
+//         Scanner scn =new Scanner(System.in);
+//         String str = scn.nextLine();
+
+//         Stack < Character > st =new Stack <> ();
+//         for(int i=0; i<str.length(); i++){
+//             char ch = str.charAt(i);
+//             if(ch=='(' || ch == '{' || ch =='['){
+//                 st.push(ch);
+//             }else if(ch == ')' ){
+//                 if(st.size()==0){
+//                     System.out.println(false);
+//                     return;
+//                 }else if(st.peek())
+//             }
+//         }
+//     }
+// }
