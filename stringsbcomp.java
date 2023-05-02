ac80036b363fc61ccc6041b7ee4a12f04bdd69ec
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-05-02 07:14:03 +0530

    .

---
+++ stringsbcomp.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class stringsbcomp {
+    public static void main(String[]args){
+        int n= 100000;
+
+        long start = System.currentTimeMillis();
+
+        // String s = "";
+        // for(int i=0; i<n; i++){
+        //     s+=i;
+        // }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++){
+            sb.append(i);
+        }
+        long end = System.currentTimeMillis();
+        long duration= end -start;
+        System.out.println(duration);
+    }
+}
