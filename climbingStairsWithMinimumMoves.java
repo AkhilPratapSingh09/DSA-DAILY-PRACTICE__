0f0bdd08023ec16467d4d04284dc24da5ac4895b
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-05-13 21:26:18 +0530

    Update climbingStairsWithMinimumMoves.java

--- climbingStairsWithMinimumMoves.java
+++ climbingStairsWithMinimumMoves.java
@@ -1,6 +1,12 @@
 import java.util.*;
 public class climbingStairsWithMinimumMoves {
     public static void main(String[]args){
-        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int []arr = new int[n];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
     }    
 }
