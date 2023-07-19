8818963c0ab3f480712085dfaa64602020303473
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-07-19 21:18:50 +0530

    .

--- stack/validParenthesis.java
+++ stack/validParenthesis.java
@@ -6,7 +6,26 @@ public class validParenthesis {
             char ch= str.charAt(i);
             if(ch=='(' || ch=='{' || ch=='['){
                 s.push(ch);
+            }else{
+                if(s.isEmpty()){
+                    return false;
+                }
+                if((s.peek()== '(' && ch==')') || (s.peek()== '{' && ch=='}') || (s.peek() =='[' && ch==']')){
+                    s.pop();
+                }
+                else{
+                    return false;
+                }
             }
         }
+        if(s.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        String str="{({})}";
+        System.out.println(isValid(str));
     }
 }
